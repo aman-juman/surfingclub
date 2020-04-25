4865e223868e089cc4c0b4785d3f0ec0fe557828
Aman89Django <87711441333@mail.ru>
2020-04-26 01:00:07 +0600

    add finish sliders

--- js/shop-slider.js
+++ js/shop-slider.js
@@ -8,43 +8,13 @@ $(document).ready(function(){
         autoplay: true,
     });
 
+    $('#shopSliderLeft').click(function() {
+        shopSlider.trigger('prev.owl.carousel',);
+    })
 
-
-
-
-
-
-
-    // const headerSlider = $('.owl-carousel');
-    // headerSlider.on('initialize.owl.carousel initialized.owl.carousel', function(event){
-    //     $('.slide-controls-number__active').text(event.item.index + 1)
-    //     $('.slide-controls-number__total').text(event.item.count)
-    //
-    // });
-    //
-    // headerSlider.owlCarousel({
-    //     items: 1,
-    //     // loop: true,
-    //     dots: false,
-    //     smartSpeed: 2000,
-    //     autoplay: true,
-    // });
-    //
-    // $('#headerSliderLeft').click(function() {
-    //     console.log('Left');
-    //     headerSlider.trigger('prev.owl.carousel',);
-    // })
-    //
-    // $('#headerSliderRight').click(function () {
-    //     console.log('Right');
-    //     headerSlider.trigger('next.owl.carousel');
-    // })
-    //
-    // headerSlider.on('changed.owl.carousel', function(event){
-    //     $('.slide-controls-number__active').text(event.item.index + 1)
-    //     $('.slide-controls-number__total').text(event.item.count)
-    //
-    // });
+    $('#shopSliderRight').click(function () {
+        shopSlider.trigger('next.owl.carousel');
+    })
 
 });
 
