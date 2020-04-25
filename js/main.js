44d247ae53d7f380aec23646253ac9fb5a757364
Aman89Django <87711441333@mail.ru>
2020-04-25 23:22:25 +0600

    add header slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-$(document).ready(function(){
-
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        loop: true,
-        dots: false,
-        smartSpeed: 2000,
-        autoplay: true,
-    });
-});
+console.log('Hello Sir!');
